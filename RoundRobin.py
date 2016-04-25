df1519e8290eae5520d04193d74c4c188d7a0744
sonicaj <waqarahmedjoyia@live.com>
2016-04-25 20:11:24 +0500

    Update RoundRobin.py

--- RoundRobin.py
+++ RoundRobin.py
@@ -43,7 +43,7 @@ class WaitingQueue:
         for d in self.queue:
             if self.queue[i].waitingFinishTime == systemTime:
                 return True
-        i += 1
+            i += 1
         return False
     def dequeue(self):
         self.queue[0].cpuRemainingTime = self.queue[0].cpuTime if self.queue[0].waitingFinishTime == systemTime else self.queue[0].cpuRemainingTime
